a2af8f4e91b586bf64f336078c31b60b288daf20
srmocher <srmocher@email.arizona.edu>
2018-02-24 11:32:32 -0700

    Implemented quotient cost for nodes

--- mlnwst.py
+++ mlnwst.py
@@ -55,12 +55,37 @@ def compute_quotient_cost(graph,trees,node):
     # sort distances from node to all trees and then take subsets of trees
     distances.sort(key=lambda x:x.dist)
 
-    # compute min cost node spider ratio
+    # compute min cost node spider ratio - consider subsets of atleast size 2
+    subset = trees[0:2]
+
+    min_spider_ratio = (node['weight'] + distances[0]['distance'] + distances[0]['distance'])/2
+    min_subset = subset
+    i = 2
+    while i < len(trees):
+        subset.append(trees[i])
+        tree_distance = 0
+        for j in range(0,i+1):
+            tree_distance = tree_distance + distances[i]['distance']
+        spider_ratio = (node['weight'] + tree_distance)/i
+        if spider_ratio < min_spider_ratio:
+            min_spider_ratio = spider_ratio
+            min_subset = subset
+
+    return min_subset,min_spider_ratio
+
 
 def iterate_steiner(graph,trees):
     graph_nodes = list(graph.nodes)
+    min_ratio = float("inf")
+    min_node = None
+    min_subset_trees = None
     for graph_node in graph_nodes:
-        compute_quotient_cost(graph,trees,graph_node)
+        subset,ratio = compute_quotient_cost(graph,trees,graph_node)
+        if ratio < min_ratio:
+            min_ratio = ratio
+            min_subset_trees = subset
+            min_node = graph_node
+    return min_node,min_subset_trees
 
 def approximate_steiner(graph,terminals):
     trees = []
